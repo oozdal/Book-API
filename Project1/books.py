76ed49056a46eb03da05cc4f67112fb8af86b057
Ozer, Ozdal <ozerozdal@gmail.com>
2024-01-15 20:10:11 -0500

    adding path parameters

--- Project1/books.py
+++ Project1/books.py
@@ -16,3 +16,15 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/mybook")
+async def read_all_books():
+    return {'book_title': 'My favorite book!'}
+
+
+@app.get("/books/{book_title}")
+async def read_all_books(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
