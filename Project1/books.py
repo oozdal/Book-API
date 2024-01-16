47ed4637984eca3d4b7cbd0449581791b608d86b
Ozer, Ozdal <ozerozdal@gmail.com>
2024-01-15 21:07:31 -0500

    adding some query parameters

--- Project1/books.py
+++ Project1/books.py
@@ -18,13 +18,39 @@ async def read_all_books():
     return BOOKS
 
 
-@app.get("/books/mybook")
-async def read_all_books():
-    return {'book_title': 'My favorite book!'}
-
-
 @app.get("/books/{book_title}")
-async def read_all_books(book_title: str):
+async def read_book(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+
+# Get all books from a specific author using path or query parameters
+@app.get("/books/byauthor/")
+async def read_books_by_author_path(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
