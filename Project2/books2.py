001487ff68c9d519a389cf533d7bcb4f2b8b3945
Ozer, Ozdal <ozerozdal@gmail.com>
2024-01-18 16:15:33 -0500

    data validation

--- Project2/books2.py
+++ Project2/books2.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional
+
 
 app = FastAPI()
 
@@ -21,12 +23,11 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
-
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
 
 
 BOOKS = [
@@ -47,4 +48,9 @@ async def read_all_books():
 @app.post("/create_book")
 async def create_book(book_request: BookRequest):
     new_book =  Book(**book_request.dict())
-    BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
