f4baa689394b73b4cf831949f0e1296268fd32cc
Ozer, Ozdal <ozerozdal@gmail.com>
2024-01-19 19:59:24 -0500

    fetch book by rating

--- Project2/books2.py
+++ Project2/books2.py
@@ -61,6 +61,16 @@ async def read_book(book_id: int):
     for book in BOOKS:
         if book.id == book_id:
             return book
+        
+
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
 
 
 @app.post("/create_book")
